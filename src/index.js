33f52ee58cd1757c8b0a7ad2c46fba471849611a
aashraya1 <aashrayabhattarai.080@kathford.edu.np>
2025-12-06 17:47:53 +0545

    fixing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';  // App now handles routing
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
-import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
+    <BrowserRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
